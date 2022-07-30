50d23ce48f5d97006cd5a93ffa7f101a60217b28
aguilanbon <aguilanbon@gmail.com>
2022-07-31 02:01:25 +0800

    decrease size of sidebar on large screens

--- src/pages/Home/Sidebar.jsx
+++ src/pages/Home/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Sidebar() {
     return (
-        <div className='lg:w-80 md:w-16 w-0 min-h-screen flex shadow-xl bg-slate-800 text-white'>
+        <div className='lg:w-60 md:w-16 w-0 min-h-screen flex shadow-xl bg-slate-800 text-white'>
 
         </div>
     )
