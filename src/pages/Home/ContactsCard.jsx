fb18f053e27a88edd17da6ecf6d43e44f53be528
aguilanbon <aguilanbon@gmail.com>
2022-07-31 12:38:59 +0800

    contact list card reponsiveness

--- src/pages/Home/ContactsCard.jsx
+++ src/pages/Home/ContactsCard.jsx
@@ -4,21 +4,23 @@ function ContactsCard() {
     return (
         <div className='w-full flex'>
             <div className='w-full flex flex-col'>
-                <div className='w-full flex flex-row items-center justify-between p-2 hover:bg-slate-200 border-b border-slate-200'>
-                    <div className='flex items-center'>
+                <div className='w-auto flex flex-row items-center p-2 hover:bg-slate-200 border-b border-slate-200 '>
+                    <div className='md:w-1/3 w-full flex items-center'>
                         <img src="./profile.png" alt="" className='w-10 h-10' />
                         <div className='flex flex-col ml-2'>
                             <p className='text-sm font-semibold'>Ella Alolor</p>
                             <p className='text-xs'>HR Manager</p>
                         </div>
                     </div>
-                    <div className='flex'>
-                        <p className='text-xs'>ella@gmail.com</p>
+                    <div className='w-full flex md:flex-row flex-col justify-evenly'>
+                        <div className='flex'>
+                            <p className='text-xs'>ella@gmail.com</p>
+                        </div>
+                        <div className='flex'>
+                            <p className='text-xs'>+63 995-651-0890</p>
+                        </div>
                     </div>
-                    <div className='flex'>
-                        <p className='text-xs'>+63 995-651-0890</p>
-                    </div>
-                    <div className='flex'>
+                    <div className='w-auto flex md:flex-row flex-col justify-end'>
                         <div className='p-2'>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-yellow-500 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
