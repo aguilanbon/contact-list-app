4c96feae82069130e4bd57905690444f27456ac0
aguilanbon <aguilanbon@gmail.com>
2022-07-31 10:22:04 +0800

    change sidebar behaviour on large screens

--- src/pages/Home/Sidebar.jsx
+++ src/pages/Home/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Sidebar() {
     return (
-        <div className='lg:w-60 md:w-16 w-0 min-h-screen flex shadow-xl bg-slate-800 text-white'>
+        <div className='xl:w-60 md:w-16 w-0 min-h-screen flex shadow-xl bg-slate-800 text-white'>
 
         </div>
     )
