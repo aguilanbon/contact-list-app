d51618a2f67e47ac2c657a00ab5df9c1e48d257e
aguilanbon <aguilanbon@gmail.com>
2022-08-01 09:26:38 +0800

    user create get input state

--- src/pages/Home/Modal/Create.jsx
+++ src/pages/Home/Modal/Create.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import ModalContext from '../../../helpers/ModalContext'
 
 function Create() {
 
     const { setOpenModalType } = useContext(ModalContext)
+
+    const [createContact, setCreateContact] = useState({
+        fName: '',
+        lName: '',
+        email: '',
+        address: '',
+        phone: null,
+        bday: null,
+    })
+
+    const handleAction = (e) => {
+        setCreateContact(prev => ({ ...prev, [e.target.name]: e.target.value }))
+    }
+
+    console.log(createContact);
 
     return (
         <div className='md:w-1/2 lg:w-1/2 xl:w-1/4 w-full mx-6 h-fit bg-white rounded-lg relative mt-10 '>
@@ -20,24 +35,24 @@ function Create() {
                 <div>
                     <form action="" className='w-full flex flex-col mt-2'>
                         <div className='w-auto flex flex-col md:flex-row mb-1'>
-                            <input type="text" placeholder='First name' className='border w-full border-slate-200 text-md py-1 px-2 rounded-md outline-8 outline-blue-400 md:mr-2 mr-0' />
-                            <input type="text" placeholder='Last name' className='w-full mt-1 md:mt-0 border border-slate-200 text-md py-1 px-2 rounded-md outline-8 outline-blue-400' />
+                            <input type="text" placeholder='First name' name='fName' className='border w-full border-slate-200 text-md py-1 px-2 rounded-md outline-8 outline-blue-400 md:mr-2 mr-0' onChange={handleAction} />
+                            <input type="text" placeholder='Last name' name='lName' className='w-full mt-1 md:mt-0 border border-slate-200 text-md py-1 px-2 rounded-md outline-8 outline-blue-400' onChange={handleAction} />
                         </div>
                         <div className='w-full flex flex-row mb-1'>
-                            <input type="email" placeholder='Email' className='w-full border border-slate-200 text-md py-1 px-2 rounded-md outline-8 outline-blue-400 mt-0' />
+                            <input type="email" placeholder='Email' name='email' className='w-full border border-slate-200 text-md py-1 px-2 rounded-md outline-8 outline-blue-400 mt-0' onChange={handleAction} />
                         </div>
                         <div className='w-full lex flex-row mb-1'>
-                            <input type="text" placeholder='Address' className='w-full border border-slate-200 text-md py-1 px-2 rounded-md outline-8 outline-blue-400 mr-2' />
+                            <input type="text" placeholder='Address' name='address' className='w-full border border-slate-200 text-md py-1 px-2 rounded-md outline-8 outline-blue-400 mr-2' onChange={handleAction} />
                         </div>
                         <div className='w-full flex mb-1'>
-                            <input type="number" placeholder='Phone no.' className='w-full border border-slate-200 text-md py-1 px-2 rounded-md outline-8 outline-blue-400' />
+                            <input type="number" placeholder='Phone no.' name='phone' className='w-full border border-slate-200 text-md py-1 px-2 rounded-md outline-8 outline-blue-400' />
                         </div>
                         <div className='w-full flex flex-col mb-1'>
                             <label htmlFor="birthday">Birthday</label>
-                            <input type="date" placeholder='Birthday' className='w-full border border-slate-200 text-md py-1 px-2 rounded-md outline-8 outline-blue-400 mt-1' />
+                            <input type="date" placeholder='Birthday' name='bday' className='w-full border border-slate-200 text-md py-1 px-2 rounded-md outline-8 outline-blue-400 mt-1' onChange={handleAction} />
                         </div>
                         <div className='w-full flex flex-col py-2'>
-                            <input type="button" value="Add" className='bg-green-400 text-white py-1 px-2 rounded-md' />
+                            <input type="button" value="Add" className='bg-green-400 text-white py-1 px-2 rounded-md cursor-pointer hover:bg-green-500' />
                         </div>
                     </form>
                 </div>
