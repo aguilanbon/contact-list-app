8b376f209c5f713f84666b8775479fb0f892b009
aguilanbon <aguilanbon@gmail.com>
2022-08-03 17:43:53 +0800

    friend request layout

--- src/pages/Home/Profile/Profile.jsx
+++ src/pages/Home/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React from 'react'
+import { useState } from 'react'
 import { useEffect } from 'react'
 import { useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
@@ -18,6 +19,7 @@ function Profile() {
     const { contacts, dispatch } = useContext(ContactContext)
 
     const { users, dispatch: altDispatch } = useContext(UserContext)
+    const [friendReq, setFriendReq] = useState(null)
 
     let navigate = useNavigate()
 
@@ -43,10 +45,13 @@ function Profile() {
     useEffect(() => {
         const getUser = async (id) => {
             const response = await axios.get(`http://localhost:4000/api/users/${id}`)
+            setFriendReq(response.data.requests)
             altDispatch({ type: 'SET_USER', payload: response.data })
         }
         getUser(uid)
     }, [uid, altDispatch])
+
+
 
     return (
         <div className='w-full min-h-screen flex bg-slate-100'>
@@ -101,28 +106,50 @@ function Profile() {
                                 </div>
                             </div>
                         </div>
-                        <div className='lg:w-3/4 w-full h-fit lg:ml-5 md:ml-0 md:mt-0 mt-4 border-x border-b border-slate-200 flex-flex-col rounded-lg'>
-                            <div className='w-full flex items-center justify-between bg-slate-800 rounded-t-lg'>
-                                <div className='w-full py-2 px-4 flex items-center'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-cyan-700" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-                                    </svg>
-                                    <h1 className='text-cyan-600 ml-4 md:text-sm text-xs w-full'>My Contacts</h1>
-                                </div>
-                                <div className='flex justify-end w-full pr-4'>
-                                    <button className='p-2 text-green-400 text-sm flex items-center hover:bg-slate-700 rounded-full' onClick={() => setOpenModalType('create')}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
+                        <div className='flex flex-col lg:w-3/4 w-full h-fit'>
+                            <div className='h-fit lg:ml-5 md:ml-0 md:mt-0 mt-4 border-x border-b border-slate-200 flex-flex-col rounded-lg'>
+                                <div className='w-full flex items-center justify-between bg-slate-800 rounded-t-lg'>
+                                    <div className='w-full py-2 px-4 flex items-center'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-cyan-700" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                                         </svg>
-                                    </button>
+                                        <h1 className='text-cyan-600 ml-4 md:text-sm text-xs w-full'>My Contacts</h1>
+                                    </div>
+                                    <div className='flex justify-end w-full pr-4'>
+                                        <button className='p-2 text-green-400 text-sm flex items-center hover:bg-slate-700 rounded-full' onClick={() => setOpenModalType('create')}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className='overflow-y-scroll max-h-96'>
+                                    {contacts?.map((contact, index) => (
+                                        <ContactsCard key={index} contact={contact} />
+                                    ))}
                                 </div>
                             </div>
-                            <div className='overflow-y-scroll max-h-96'>
-                                {contacts?.map((contact, index) => (
-                                    <ContactsCard key={index} contact={contact} />
+
+
+                            <div className='w-auto h-fit lg:ml-5 md:ml-0 md:mt-0 mt-4 flex-flex-col rounded-lg mr-4'>
+                                {friendReq?.map(rq => (
+                                    <div className='w-auto flex mt-4' key={rq._id}>
+                                        <div className='flex p-2 border border-slate-200 bg-white rounded-md items-center'>
+                                            <div className='flex'>
+                                                <p className='text-xs'><strong>{rq.fName} {rq.lName}</strong> sent you a friend request</p>
+                                            </div>
+                                            <div className='flex text-white ml-2'>
+                                                <button className='text-xs rounded-md bg-green-400 p-1 mr-2'>Accept</button>
+                                                <button className='text-xs rounded-md bg-red-400 p-1'>Decline</button>
+                                            </div>
+                                        </div>
+                                    </div>
                                 ))}
                             </div>
+
+
                         </div>
+
                     </div>
                 </div>
             </div>
